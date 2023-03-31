6de29f3d2dd3a5433d40259844ac60aaa0eb11ad
Ranajit Das <dasranajit410@gmail.com>
2023-03-31 13:47:23 +0530

    Form added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,28 @@
-import logo from './logo.svg';
+import { useEffect, useState } from "react";
 import './App.css';
 
 function App() {
+  const [stamp1,setStamp1]=useState("")
+  const [stamp2,setStamp2]=useState("")
+  const [difference,setDifference]=useState("")
+  const handleSubmit=()=>{
+
+  }
   return (
     <div className="App">
-      hello
+      <form onSubmit={handleSubmit}>
+        <label>First Timestamp: 
+          <input type="text" value={stamp1} onChange={(e)=>setStamp1(e.target.value)}/>
+        </label>
+        <br/>
+        <label>Second Timestamp: 
+          <input type="text" value={stamp2} onChange={(e)=>setStamp2(e.target.value)}/>
+        </label>
+        <br/>
+        <button type="submit">Check Difference</button>
+      </form>
+      <h2>The difference in seconds</h2>
+      <h1>{difference || "No Data"}</h1>
     </div>
   );
 }
