bbebfb5ee8bd34bf49450c4bbad119e181e2f254
Ranajit Das <dasranajit410@gmail.com>
2023-03-31 14:43:47 +0530

    Final submission€

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+var moment = require('moment');  
 const cors = require("cors");
 require("dotenv").config();
 let port = process.env.PORT;
@@ -12,7 +13,13 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("This is homepage");
 });
-
+app.get("/difference", (req, res) => {
+   const {timestamp1,timestamp2}=req.query;
+   const formattedDate1 = moment(timestamp1, 'DD:MM:YYYY HH:mm:ss').format('YYYY-MM-DDTHH:mm:ss');
+   const formattedDate2 = moment(timestamp2, 'DD:MM:YYYY HH:mm:ss').format('YYYY-MM-DDTHH:mm:ss');
+   const difference=moment(formattedDate2).diff(moment(formattedDate1),'seconds')
+   res.send({difference})
+  });
 app.listen(port, async () => {
     try {
       await connection;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,27 @@
-import { useEffect, useState } from "react";
+import {useState } from "react";
+import axios from "axios"
 import './App.css';
 
 function App() {
   const [stamp1,setStamp1]=useState("")
   const [stamp2,setStamp2]=useState("")
   const [difference,setDifference]=useState("")
-  const handleSubmit=()=>{
+  const handleSubmit=async(e)=>{
+    e.preventDefault()
+    const response=await axios.get(`http://localhost:8080/difference?timestamp1=${stamp1}&timestamp2=${stamp2}`);
+    console.log(response.data)
+    setDifference(response.data.difference)
 
   }
   return (
     <div className="App">
       <form onSubmit={handleSubmit}>
         <label>First Timestamp: 
-          <input type="text" value={stamp1} onChange={(e)=>setStamp1(e.target.value)}/>
+          <input type="text" value={stamp1} placeholder="Enter in this format DD:MM:YYYY HH:MM:SS" onChange={(e)=>setStamp1(e.target.value)}/>
         </label>
         <br/>
         <label>Second Timestamp: 
-          <input type="text" value={stamp2} onChange={(e)=>setStamp2(e.target.value)}/>
+          <input type="text" value={stamp2} placeholder="Enter in this format DD:MM:YYYY HH:MM:SS" onChange={(e)=>setStamp2(e.target.value)}/>
         </label>
         <br/>
         <button type="submit">Check Difference</button>
